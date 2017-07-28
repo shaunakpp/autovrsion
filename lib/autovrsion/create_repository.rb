8673a7cc8e88b770bb896d663e1d9e80f2739600
Shaunak Pagnis <shaunak.pagnis@gmail.com>
2017-07-28 15:46:08 +0530

    removed unnecessary assignment in create_repository

--- lib/autovrsion/create_repository.rb
+++ lib/autovrsion/create_repository.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/env ruby
 module Autovrsion
+  # initializes a new git repository
   class CreateRepo
     def create(path)
-      repository = Rugged::Repository.init_at(path)
+      Rugged::Repository.init_at(path)
       puts "Repository created at directory #{path}".cyan
     end
   end
